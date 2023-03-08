4e67a594b993305426dd8be2fc253d42f6861a5e
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-03-08 15:50:14 +0100

    task 12

--- 0x01-python-if_else_loops_functions/test.py
+++ 0x01-python-if_else_loops_functions/test.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-print_last_digit = __import__('9-print_last_digit').print_last_digit
+fizzbuzz = __import__('12-fizzbuzz').fizzbuzz
 
-print_last_digit(55)
-print_last_digit(-1025)
-print_last_digit(1000)
-print_last_digit(88)
+fizzbuzz()
+print("")
