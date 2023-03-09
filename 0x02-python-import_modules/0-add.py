965c499632e41cfefc8331793f61f03273cc8dea
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-03-09 15:49:34 +0100

    task0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from add_0 import add
 if __name__ == "__main__":
-    arg1 = 1
-    arg2 = 2
-    print('{} + {} = {}'.format(arg1, arg2, add_0.add(arg1, arg2)))
+    a = 1
+    b = 2
+    print('{} + {} = {}'.format(a, b, add(a, b)))
