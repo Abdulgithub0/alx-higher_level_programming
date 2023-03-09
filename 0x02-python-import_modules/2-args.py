181bbc936ba4e76fbc4e731bb3e317e41f47add5
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-03-09 16:28:29 +0100

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import argv
+num = len(argv)
+if (num == 1):
+    print('{} arguments.'.format(0))
+elif (num == 2):
+    print('{} argument:'.format(1))
+    print('{}: {}'.format(1, argv[1]))
+else:
+    print('{} arguments:'.format(num - 1))
+    for i in range(1, num):
+        print('{}: {}'.format(i, argv[i]))
