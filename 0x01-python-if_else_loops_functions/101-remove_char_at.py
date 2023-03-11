935a8a86601ce8ea034c16dc0f5ee9656e21bbb3
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-03-11 21:43:17 +0100

    task 101

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    copy_str = ''
+    for i in range(len(str)):
+        if (i == n):
+            copy_str = ''
+        else:
+            copy_str += str[i]
+    return (copy_str)
