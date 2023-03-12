e3553c7b66fa57b67af70c597d072ecb1ca4e3b9
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-03-12 09:17:04 +0100

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     lenght = len(my_list)
     if (idx > lenght or idx < 0):
-        return (0)
+        return (None)
     for i in range(lenght):
         if i == idx:
             return my_list[i]
