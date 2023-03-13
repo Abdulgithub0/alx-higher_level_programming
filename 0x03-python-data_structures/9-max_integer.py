430299634ff1f23f1ba20fc143730c038c0eeabe
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-03-13 12:26:59 +0100

    task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+def max_integer(my_list=[]):
+    if not (my_list):
+        return (None)
+    else:
+        max_num = my_list[0]
+        for i in range(len(my_list)):
+            if (max_num < my_list[i]):
+                max_num = my_list[i]
+        return (max_num)
