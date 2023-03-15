d3a95eda3e43565d63c8d7972092caa1e2296170
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-03-15 21:39:59 +0100

    task 6

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,10 @@
+#!/bin/usr/python3
+def print_sorted_dictionary(a_dictionary):
+    sort_dict = []
+    new_dict = dict()
+    for key in a_dictionary.keys():
+        sort_dict.append(key)
+    sort_dict.sort()
+    for key in sort_dict:
+        if key in a_dictionary:
+            print('{}: {}'.format(key, a_dictionary[key]))
