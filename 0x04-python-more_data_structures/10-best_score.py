ee9f054f6de4e6756c75fe070d5fcbe14e433efb
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-03-16 23:03:27 +0100

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    best = "none"
-    for key, value in a_dictionary.items():
-        max_score = value
+    if not (a_dictionary):
+        return None
+    best = "None"
+    for key, values in a_dictionary.items():
+        scorer = key
         break
-    for key, value in a_dictionary.items():
-        if max_score < value:
-            max_score = value
-            best = key
-        else:
-            if (best == "none"):
-                best = "None"
-    return (best)
+    for keys, values in a_dictionary.items():
+        if a_dictionary[scorer] < a_dictionary[keys]:
+            scorer = keys
+            best = scorer
+    return best
--- 0x04-python-more_data_structures/tester.py
+++ 0x04-python-more_data_structures/tester.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-only_diff_elements = __import__('4-only_diff_elements').only_diff_elements
+best_score = __import__('10-best_score').best_score
 
-set_1 = { "Python", "C", "Javascript" }
-set_2 = { "Bash", "C", "Ruby", "Perl" }
-od_set = only_diff_elements(set_1, set_2)
-print(sorted(list(od_set)))
+a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+best_key = best_score(a_dictionary)
+print("Best score: {}".format(best_key))
+
+best_key = best_score(None)
+print("Best score: {}".format(best_key))
