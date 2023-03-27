58f8e48e415882f0db8463f34db3ede010fcc90f
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-03-27 21:45:22 +0100

    combine three exception classes

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,14 +3,16 @@ def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(list_length):
         try:
-            new_list.append((my_list_1[i] / my_list_2[i]))
+            added = (my_list_1[i] / my_list_2[i])
         except TypeError:
-            new_list.append(0)
+            added = 0
             print("wrong type")
         except IndexError:
-            new_list.append(0)
+            added = 0
             print("out of range")
         except ZeroDivisionError:
-            new_list.append(0)
+            added = 0
             print("division by 0")
+        finally:
+            new_list += [added]
     return new_list
