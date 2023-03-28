e4fa58854779f1b79dbbeba1f3a675dcb760b947
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-03-28 21:46:54 +0100

    implemented my_print method that prints into stdout the square with the character # for the class Square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""Creating a class square.
+    self: reference the name of the object of instance 'Square'.
+"""
+
+
+class Square:
+    """defining Class square body."""
+    def __init__(self, __size=0):
+        """
+            Args:
+                 __size: unaccessible/private field of the init method
+        """
+        if (isinstance(__size, int)):
+            if (__size < 0):
+                raise ValueError("size must be >= 0")
+            self.__size = __size
+        else:
+            raise TypeError("size must be an integer")
+
+    @property
+    def size(self):
+        """
+            getter method  that return a copy of
+            value of  private attribute __size.
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """
+            setter method that change the value of private attribute __size.
+        """
+        if (isinstance(value, int)):
+            if (value < 0):
+                raise ValueError("size must be >=0")
+            self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """
+            function area is a public method of class \
+            Square that compute the area of the square.
+        """
+        return (self.__size * self. __size)
+
+    def my_print(self):
+        """
+            function that prints in stdout the square with the character '#'
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print('#' * self.__size)
