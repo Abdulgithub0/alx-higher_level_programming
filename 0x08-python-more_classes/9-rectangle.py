ded5ceab5fff591fec3a1b69b2c9aae08c3492f1
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-04-03 21:37:32 +0100

    implemented a classmethod called square

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -105,6 +105,8 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
+        """return a new rectangle instance with widht and height set to size"""
+
         cls.__width = size
         cls.__height = size
         return cls(cls.__width, cls.__height)
