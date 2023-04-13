4ea26dabb07a20dc8b3b528c1fd40e5bbe94b85a
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-04-13 13:24:25 +0100

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""creating student class"""
+import json
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if (type(attrs) == list):
+            dic = {}
+            d = self.__dict__
+            for keys, val in d.items():
+                if keys in attrs:
+                    dic[keys] = val
+            return dic
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        dic = self.__dict__
+        for key, val in json.items():
+            for k, v in dic.items():
+                if key == k:
+                    dic[key] = val
