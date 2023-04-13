08cad54d2ba0e201d8f3e41dccf07a430d7f98a5
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-04-13 12:30:30 +0100

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""creating student class"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if (type(attrs) == list):
+            dic = {}
+            d = self.__dict__
+            for keys, val in d.items():
+                for i in attrs:
+                    if keys == i:
+                        dic[keys] = val
+            return dic
+        else:
+            return self.__dict__
