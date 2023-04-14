a96272da594c73ccba674948b3ce109a87115127
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-04-14 19:48:48 +0100

    check for instances

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """check for class instance"""
 
+
 def inherits_from(obj, a_class):
     """check for obj or inheritances"""
     if (issubclass(type(obj), a_class) and type(obj) is not a_class):
