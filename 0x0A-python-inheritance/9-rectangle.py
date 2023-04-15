addeb45f468730033b25bd9c43aff860d2c91312
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-04-15 11:47:03 +0100

    task 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""creating a class"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """implementing a Rectangle class"""
+    def __init__(self, width, height):
+        """initializing class Rectangle
+
+        Args:
+            width: width of rectangle in int type
+            height: height of the reactangle in int type
+        """
+        self.integer_validator('width', width)
+        self.__width = width
+        self.integer_validator('height', height)
+        self.__height = height
+
+    def area(self):
+        """calculate the area of the Reactangle"""
+        return self.__width * self.__height
+    
+    def __str__(self):
+        """invoke call for print() and str()"""
+        return '[Rectangle] {}/{}'.format(self.__width, self.__height)
