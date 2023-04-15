d9ba4b7d39c4b3947efe51145055a5fa7c294879
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-04-15 11:57:24 +0100

    task 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -20,4 +20,4 @@ class Square(Rectangle):
 
     def __str__(self):
         """invoke call for print() and str()"""
-        return '[Rectangle] {}/{}'.format(self.__size, self.__size)
+        return '[Square] {}/{}'.format(self.__size, self.__size)
