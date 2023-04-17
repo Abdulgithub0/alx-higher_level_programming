f190deae48417f2b0598c971f30038c454b2672c
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-04-17 21:58:29 +0100

    working on test cases

---
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
+class TestBase_init(unittest.TestCase):
+    """test for Base class __init__"""
+
+    def testNone_id(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertNotEqual(b1.id, b2.id, msg='check testNone_id')
+
+    def testMixed_id(self):
+        b3 = Base(12)
+        b4 = Base()
+        self.assertGreater(b3.id, b4.id, msg='check testMixed_id')
+
+    def testId_setter(self):
+        b5 = Base()
+        b5.id = 10
+        self.assertEqual(b5.id, 10, msg='testId_setter')
+
+if __name__ == '__main__':
+    unittest.main()
