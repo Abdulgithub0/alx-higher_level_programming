79450ebbdc1c88544fdc633504fdd0b442bfd7c7
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-04-19 14:18:14 +0100

    fixing bug

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 """
-    implementing class Rectangle that
-    inherit from a class called Base
+    implementing class Rectangle thatd Base
 """
 
 
@@ -12,8 +11,7 @@ from models.base import Base
 class Rectangle(Base):
     """
     A class representing a rectangle.
-
-    Parameters:
+    Args:
         width (int): The width of the rectangle.
         height (int): The height of the rectangle.
         x (int): The x-coordinate of the top-left corner of the rectangle.
