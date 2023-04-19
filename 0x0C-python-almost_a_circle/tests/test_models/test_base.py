8ee8b01e2927b98b5d38276ae6df75b5c17afea5
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-04-19 12:36:54 +0100

    fixing bug

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,28 +1,43 @@
 #!/usr/bin/python3
+
+
+"""implementing unittest cases for Class Base"""
+
+
 import unittest
 from models.base import Base
-from models.rectangle import Rectangle
-from models.square import Square
 
 
 class TestBase_init(unittest.TestCase):
-    """test for Base class __init__"""
+    """defining unittest method for 
+        each base class edge cases
+    """
 
     def testNone_id(self):
+        """test for empty instances
+        initialization with empty id value
+        """
         b1 = Base()
         b2 = Base()
-        self.assertEqual(b1.id, b2.id - 1, msg='check testNone_id')
+        self.assertEqual(b1.id, b2.id - 1)
 
     def testMixed_id(self):
+        """for both empty and non-empty instance declaration"""
         b3 = Base(12)
         b4 = Base()
-        self.assertNotEqual(b3.id, b4.id, msg='check testMixed_id')
+        self.assertNotEqual(b3.id, b4.id)
 
     def testId_setter(self):
+        """test for setter method"""
         b5 = Base()
         b5.id = 10
-        self.assertEqual(b5.id, 10, msg='testId_setter')
+        self.assertEqual(b5.id, 10)
 
+    def testid_getter(self):
+        """test for getter method of the attr id"""
+        b6 = Base()
+        get = b6.id
+        self.assertEqual(get, 5)
 
 if __name__ == '__main__':
     unittest.main()
