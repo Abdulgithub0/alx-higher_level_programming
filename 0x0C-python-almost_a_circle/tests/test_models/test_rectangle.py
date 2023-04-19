94252a51b5a0023fc0d7156d09d0f9424b3a2d47
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-04-19 13:58:00 +0100

    fixing bug

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -10,16 +10,33 @@ from models.rectangle import Rectangle
 class TestRectangle(unittest.TestCase):
     """defining test methods for diff edge cases"""
 
-    def testInheritId(self):
-        """test for inherited id"""
-        r1 = Rectangle(10, 2)
-        self.assertEqual(r1.id, 6)
-
-    def test_Initializer(self):
+    def test_initializer(self):
         """testing for initializer method"""
-        r2 = Rectangle(2, 10, 5, 6, 100)
-        self.assertEqual(r2.width, 2)
-        self.assertEqual(r2.height, 10)
-        self.assertEqual(r2.x, 5)
-        self.assertEqual(r2.y, 6)
-        self.assertEqual(r2.id, 100)
+        r1 = Rectangle(2, 10, 5, 6, 100)
+        self.assertEqual(r1.width, 2)
+        self.assertEqual(r1.height, 10)
+        self.assertEqual(r1.x, 5)
+        self.assertEqual(r1.y, 6)
+        self.assertEqual(r1.id, 100)
+
+    def test_terror(self):
+        """test for typeError"""
+        with self.assertRaises(TypeError):
+            r2 = Rectangle("5", 10, 2, 3, 1)
+
+    def test_position_argument(self):
+        """test for parameter overloading"""
+        with self.assertRaises(TypeError):
+            r3 =  Rectangle(5, 10, 2, 3, 1, 100)
+
+    def test_empty_arg(self):
+        """test for empty initialization"""
+        with self.assertRaises(TypeError):
+            r4 = Rectangle()
+    def test_verror(self):
+        """test for ValuError"""
+        with self.assertRaises(ValueError):
+            r4 = Rectangle(5, 10, 2, -3, 1)
+
+if __name__ == '__main__':
+    unittest.main()
