d4ca27aa60f34f4fd33bc67a192d335c88c15ca0
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-05-22 14:08:27 +0100

    task 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,4 +3,4 @@
 -- 	Each record  display: tv_shows.title - tv_show_genres.genre_id
 -- 	Results set are sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 -- 	The database name will be passed as an argument of the mysql command
-SELECT s.title, t.genre_id FROM tv_show_genres t, tv_shows s WHERE t.genre_id = s.id ORDER BY s.title, t.genre_id;
+SELECT s.title, t.genre_id FROM tv_show_genres t, tv_shows s WHERE s.id = t.show_id ORDER BY s.title, t.genre_id;
