2a834184277c4de12bc922cdfa3a637e501e483c
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-05-22 11:21:29 +0100

    task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- a script that lists all cities contained in the database hbtn_0d_usa.
-SELECT c.id, c.name, s.name FROM cities c, states s ORDER BY c.id;
+SELECT c.id, c.name, s.name FROM cities c, states s WHERE c.state_id = s.id ORDER BY c.id;
