786f661a0a0b610ed97f0d15c3dd20ecc780d275
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-06-13 23:36:28 +0100

    working with command line argument in js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,16 +3,11 @@
 /*
  * a script that prints a message depending of the number of arguments passed
  */
-const argLen = require('node:process').argv.length; 
-if (argLen === 2)
-{
-	console.log('Argument found');
-}
-else if (argLen > 2)
-{
-	console.log('Arguments found');
-}
-else
-{
-	console.log('No argument');
+const argLen = require('node:process').argv.length;
+if (argLen === 3) {
+  console.log('Argument found');
+} else if (argLen > 3) {
+  console.log('Arguments found');
+} else {
+  console.log('No argument');
 }
