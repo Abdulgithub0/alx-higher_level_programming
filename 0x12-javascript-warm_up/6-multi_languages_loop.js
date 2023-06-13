495e58899ddeba1af0e3823718f88837781442f8
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-06-14 00:28:39 +0100

    working with array and loop in javascript

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+/*
+ * a script that prints 3 sentences by using an array of string and a loop
+ */
+const str = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < str.length; i++) {
+  console.log('%s', str[i]);
+}
