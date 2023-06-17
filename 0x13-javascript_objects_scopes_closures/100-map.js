812ba79f927d9f096e132ffb2f5eebbda09c6c0d
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-06-17 17:17:04 +0100

    working with map() in js

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+/**
+ *  script that imports an array and computes a new array by multiply each element by it index.
+ */
+const list = require('./100-data').list;
+const newList = list.map(function (e, i) { return (e * i); });
+console.log(list);
+console.log(newList);
