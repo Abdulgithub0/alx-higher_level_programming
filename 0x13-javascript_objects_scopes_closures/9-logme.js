c7bbfe3638c914c8cf950cb0d4616c4df07d17cc
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-06-17 15:55:09 +0100

    everything in js is an Object

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -3,14 +3,8 @@
 /**
  *  a function that prints the number of arguments already printed and the new argument value. (see example below)
  */
-
+exports.logMeCounter = 0;
 exports.logMe = function (item) {
-  if (exports.logMeCounter) {
-    console.log(`${exports.logMeCounter}: ${item}`);
-    exports.logMeCounter++;
-  } else {
-    exports.logMeCounter = 0;
-    console.log(`${exports.logMeCounter}: ${item}`);
-    exports.logMeCounter++;
-  }
+  console.log(`${exports.logMeCounter}: ${item}`);
+  exports.logMeCounter++;
 };
