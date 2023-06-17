026eb458ecf588ae70ba9c6cf1bd81ba94146f1a
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-06-17 11:35:36 +0100

    added width and height data properties to class Rectangle

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+/**
+ * Class Rectangle represent a protolytic object for Rectangle shape
+ */
+
+class Rectangle {
+  /* adding properties */
+
+  width;
+  height;
+
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
