d5ee07382cb7d32bbda71992fb0c4b4c8a1146a2
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-06-18 22:59:44 +0100

    query that match the first 'N' character in state Table

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+script that lists all states from the database hbtn_0e_0_usa when state[0] = N
+"""
+
+
+if "__main__" == __name__:
+    import MySQLdb as mysql
+    from sys import argv
+
+    use_db = mysql.connect(host="localhost", user=argv[1], passwd=argv[2],
+                           database=argv[3])
+    navigate = use_db.cursor()
+    navigate.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    for i in navigate.fetchall():
+        print(i)
+    navigate.close()
+    use_db.close()
