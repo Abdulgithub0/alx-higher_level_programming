bef5fbe56633f562bd121ff4b6ed6e3616183689
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-06-18 22:36:39 +0100

    using python3 db api 2 module to interact with mysql server

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,8 @@ a script that lists all states from the database hbtn_0e_0_usa
 import MySQLdb as mysql
 from sys import argv
 
-use_db = mysql.connect(host="localhost", user=argv[1], passwd=argv[2], database=argv[3])
+use_db = mysql.connect(host="localhost", user=argv[1], passwd=argv[2],
+                       database=argv[3])
 navigate = use_db.cursor()
 navigate.execute("SELECT * FROM states ORDER BY id ASC")
 for i in navigate.fetchall():
