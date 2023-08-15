c1c84e940dc4c51b69e0d73473d8a721becdc3fd
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-08-15 15:55:19 +0100

    refactor 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -8,8 +8,8 @@ class Rectangle {
   /* adding properties */
   constructor (w, h) {
     if (w >= 1 && h >= 1) {
-	    this.width = w;
-	    this.height = h;
+      this.width = w;
+      this.height = h;
     }
   }
 }
