e880b8f1c9e0932111129f9c743f60a0a08f153e
Bello Abdulsamad <117740814+Abdulgithub0@users.noreply.github.com>
2023-08-15 15:18:33 +0100

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-/**
- *  script that imports an array and computes a new array by multiply each element by it index.
- */
-const list = require('./100-data').list;
-const newList = list.map(function (e, i) { return (e * i); });
-console.log(list);
-console.log(newList);
