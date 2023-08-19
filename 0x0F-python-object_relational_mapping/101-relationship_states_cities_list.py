5402b5e50b626a7a8ebcc747c325d43a0275a3f9
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-08-19 19:08:49 +0100

    display instances of State and its assiociated cities

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""
+ a script that lists all State objects, and corresponding
+ City objects, contained in the database hbtn_0e_101_usa
+"""
+
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from relationship_state import Base, State
+    from relationship_city import City
+    from sys import argv
+
+    u = argv[1]
+    p = argv[2]
+    ht = f"@localhost:3306/{argv[3]}"
+
+    # create engine
+    engine = create_engine(f"mysql+mysqldb://{u}:{p}{ht}", pool_pre_ping=True)
+
+    # mapped classes to their corresponding tables then store on engine
+    Base.metadata.create_all(engine)
+
+    # open communication channel to the dialect
+    session = sessionmaker(bind=engine)()
+
+    res = session.query(State).order_by(State.id).all()
+    for s in res:
+        print(f"{s.id}:  {s.name}")
+        for c in s.cities:
+            print(f"     {c.id}: {c.name}")
+    session.commit()
+    session.close()
