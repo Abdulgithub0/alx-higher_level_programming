b270d7dc936be1e31efb45915c37dedebd3f624b
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-08-19 11:40:50 +0100

    fetch all instances of class State from mapped table states

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -16,8 +16,13 @@ db = argv[3]
 ht = "@localhost:3306"
 engine = create_engine(f"mysql+mysqldb://{u}:{p}{ht}/{db}", pool_pre_ping=True)
 
+# temp store the mapped object(states--State) & its MD setting on engine
+Base.metadata.create_all(engine)
+
 # bind the configure info to this .py process and connect
 session = sessionmaker(bind=engine)()
 res = session.query(State).order_by(State.id).all()
 for obj in res:
     print(f"{obj.id}: {obj.name}")
+session.commit()
+session.close()
