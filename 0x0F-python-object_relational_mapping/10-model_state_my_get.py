fe90c399e90a6d1de24540af9aa2b4dd9e3905cc
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-08-19 13:06:22 +0100

    search a State instance with arg passed as command line'

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+a script that lists all State objects that contain the letter 'a' hbtn_0e_6_usa
+"""
+if __name__ == "__main__":
+    from model_state import State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from sys import argv
+
+    # Get configure info
+    u = argv[1]
+    p = argv[2]
+    db = argv[3]
+    ht = "@localhost:3306"
+    engine = create_engine(f"mysql+mysqldb://{u}:{p}{ht}/{db}",
+                           pool_pre_ping=True)
+
+    # bind the configure info to this .py process and connect
+    session = sessionmaker(bind=engine)()
+    s = argv[4]
+    res = session.query(State).filter(State.name == s).all()
+    if res:
+        for obj in res:
+            print(f"{obj.id}")
+    else:
+        print("Not found")
+    session.commit()
+    session.close()
