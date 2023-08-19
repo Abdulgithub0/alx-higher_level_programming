00d1ee93764c036eccd2822d61f8fe8cb73da4a2
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-08-19 11:44:25 +0100

    fetch all instances of class State from mapped table states

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,26 +3,27 @@
 """
 a script that lists all State objects from the database hbtn_0e_6_usa
 """
+if __name__ == "__main__":
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from sys import argv
 
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sys import argv
+    # Get configure info
+    u = argv[1]
+    p = argv[2]
+    db = argv[3]
+    ht = "@localhost:3306"
+    engine = create_engine(f"mysql+mysqldb://{u}:{p}{ht}/{db}",
+                           pool_pre_ping=True)
 
-# Get configure info
-u = argv[1]
-p = argv[2]
-db = argv[3]
-ht = "@localhost:3306"
-engine = create_engine(f"mysql+mysqldb://{u}:{p}{ht}/{db}", pool_pre_ping=True)
+    # temp store the mapped object(states--State) & its MD setting on engine
+    # Base.metadata.create_all(engine)
 
-# temp store the mapped object(states--State) & its MD setting on engine
-Base.metadata.create_all(engine)
-
-# bind the configure info to this .py process and connect
-session = sessionmaker(bind=engine)()
-res = session.query(State).order_by(State.id).all()
-for obj in res:
-    print(f"{obj.id}: {obj.name}")
-session.commit()
-session.close()
+    # bind the configure info to this .py process and connect
+    session = sessionmaker(bind=engine)()
+    res = session.query(State).order_by(State.id).all()
+    for obj in res:
+        print(f"{obj.id}: {obj.name}")
+    session.commit()
+    session.close()
