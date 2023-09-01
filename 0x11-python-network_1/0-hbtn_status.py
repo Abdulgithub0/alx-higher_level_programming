975ce3f7a1ccf9a942356b6446d9e04b30d8e2d7
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-09-01 20:39:41 +0100

    a script that fetch content

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
             print("Body response:")
             print("\t- type: {}".format(type(r)))
             print("\t- content: {}".format(r))
-            print("\t- utf-8 content: {}".format(r.decode("utf-8")))
+            print("\t- utf8 content: {}".format(r.decode("utf-8")))
     except req.URLError as e:
         pass
