042abe07955a99bd4a8f8addaa398f666e1201d1
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-09-01 22:25:50 +0100

    print a variable from header section of respons

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    The script return the value of X-Request-Id
+    found in the response header from server
+"""
+
+import requests as req
+from sys import argv
+
+
+def browser(url):
+    try:
+        with req.get(url) as resp:
+            print(resp.headers.get("X-Request-Id"))
+    except req.exceptions.RequestException as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    browser(argv[1])
