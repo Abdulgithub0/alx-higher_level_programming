d3651eb832917316e5adc7ce2dea63dab3e3bab2
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-09-01 21:57:03 +0100

    task 3: detect http error and send it corresponding status code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 a Python script that takes in a URL, sends a request to the URL
-and displays the body of the response (decoded in utf-8)
+and displays the body of the response  (decoded in utf-8)
 """
 if __name__ == "__main__":
     from urllib import request, parse, error
