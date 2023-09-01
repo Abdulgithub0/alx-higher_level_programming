368fb54b23546c89a8a3ef2eb921fc03a044a5e8
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-09-01 20:36:31 +0100

    a script that fetch content

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """a Python script that fetches https://alx-intranet.hbtn.io/status"""
 if __name__ == "__main__":
-    import urllib.request as l
+    import urllib.request as req
     try:
-        with l.urlopen("https://alx-intranet.hbtn.io/status") as res:
+        with req.urlopen("https://alx-intranet.hbtn.io/status") as res:
             r = res.read()
             print("Body response:")
             print("\t- type: {}".format(type(r)))
             print("\t- content: {}".format(r))
             print("\t- utf-8 content: {}".format(r.decode("utf-8")))
-    except l.URLError as e:
+    except req.URLError as e:
         pass
