5ffac08dd13b4442389cf741941b11034c362e6c
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-09-01 21:18:28 +0100

    return a variable value in the header section of server response

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    The script return the value of X-Request-Id
+    found in the response header from server
+"""
+if __name__ == "__main__":
+    import urllib.request as req
+    from sys import argv
+
+    def browser(url):
+        try:
+            with req.urlopen(url) as resp:
+                print(dict(resp.headers._headers).get("X-Request-Id"))
+        except req.error.URLError as e:
+            return
+    browser(argv[1])
