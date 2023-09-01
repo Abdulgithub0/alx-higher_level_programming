6b7a61f4cbfeca31ee1e14f8682763d1276bf796
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-09-01 20:25:41 +0100

    a script that fetch content

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     with client.urlopen("https://alx-intranet.hbtn.io/status") as res:
         content = res.read()
         print("Body response:$")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf-8 content: {}".format(content.decode("utf-8")))
+        print("\t- type: {}$".format(type(content)))
+        print("\t- content: {}$".format(content))
+        print("\t- utf-8 content: {}$".format(content.decode("utf-8")))
