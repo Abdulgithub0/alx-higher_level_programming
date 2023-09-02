dc5eacfe6eedb814572889001dd520d9d3ea0a49
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-09-02 10:48:32 +0100

    fixing

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -12,13 +12,11 @@ def latest_commits(repos, username):
     url = "https://api.github.com/repos/{}/{}/commits".format(
           repos, username)
     resp = req.get(url, params={"per_page": 10})
-    try:
-        json_data = resp.json()
+    json_data = resp.json()
+    if (json_data):
         for data in json_data:
-            s = data["sha"]
+            s = data.get("sha")
             name = data.get("commit").get("author").get("name")
             print(f"{s}: {name}")
-    except KeyError as e:
-        print(e)
 if __name__ == "__main__":
     latest_commits(argv[1], argv[2])
