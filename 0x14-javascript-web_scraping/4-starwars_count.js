c651644bd1e4e9384c7ea750a5c39a4be586c62b
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-09-27 02:02:11 +0100

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -8,9 +8,17 @@
 const req = require('request');
 
 if (process.argv.length >= 3) {
-  const url = process.argv[2].replace('/films', '/people/18');
+  const url = process.argv[2];
   req.get(url, (err, resp, body) => {
-    const obj = JSON.parse(body);
-    if (!err) { console.log(obj.films.length); }
+    if (!err) {
+      const obj = JSON.parse(body);
+      let cha = 0;
+      for (let i = 0; i < obj.results.length; i++) {
+        for (let j = 0; j < obj.results[i].characters.length; j++) {
+	  if (obj.results[i].characters[j].endsWith('18/')) cha++;
+        }
+      }
+      console.log(cha);
+    }
   });
 }
